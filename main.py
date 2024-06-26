135ca4a9a9f012f561f88e7c50a0ae5b4aae3ca5
minyuanzhu <56624982+JackZzZ233@users.noreply.github.com>
2024-06-26 23:39:06 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,155 +1,223 @@
 import torch
+from torch import optim
 from torch.cuda.amp import GradScaler
 from inference_utils import SSIMLoss,psnr,lpips_fn,save_img_tensor
 from inference_models import get_init_noise, get_model,from_noise_to_image
 from inference_image0 import get_image0
+from predict import predict_image_tensor
 import argparse
 import numpy as np
 import complexity
 import cv2
 from noise import get_cifar10_dataloaders
 from unet_model import UNet
+import multiprocessing
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_selection", default="", type=str, help="The path of dev set.")
+    parser.add_argument("--distance_metric", default="l2", type=str, help="The path of dev set.")
+    parser.add_argument("--model_type", default="ddpm_cifar10", type=str, help="The path of dev set.")
+    parser.add_argument("--model_path_", default=None, type=str, help="The path of dev set.")
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--input_selection", default="", type=str, help="The path of dev set.")
-parser.add_argument("--distance_metric", default="l2", type=str, help="The path of dev set.")
-parser.add_argument("--model_type", default="ddpm_cifar10", type=str, help="The path of dev set.")
-parser.add_argument("--model_path_", default=None, type=str, help="The path of dev set.")
+    parser.add_argument("--lr", default=1e-2, type=float, help="")
+    parser.add_argument("--dataset_index", default=None, type=int, help="")
+    parser.add_argument("--bs", default=8, type=int, help="")
+    parser.add_argument("--write_txt_path", default=None, type=str, help="The path of dev set.")
+    parser.add_argument("--num_iter", default=2000, type=int, help="The path of dev set.")
+    parser.add_argument("--strategy", default="mean", type=str, help="The path of dev set.")
+    parser.add_argument("--mixed_precision", action="store_true", help="The path of dev set.")
+    parser.add_argument("--sd_prompt", default=None, type=str, help="The path of dev set.")
+    parser.add_argument("--input_selection_url", default=None, type=str, help="The path of dev set.")
+    parser.add_argument("--input_selection_name", default=None, type=str, help="The path of dev set.")
+    parser.add_argument("--input_selection_model_type", default=None, type=str, help="The path of dev set.")
+    parser.add_argument("--input_selection_model_path", default=None, type=str, help="The path of dev set.")
 
-parser.add_argument("--lr", default=1e-2, type=float, help="")
-parser.add_argument("--dataset_index", default=None, type=int, help="")
-parser.add_argument("--bs", default=8, type=int, help="")
-parser.add_argument("--write_txt_path", default=None, type=str, help="The path of dev set.")
-parser.add_argument("--num_iter", default=2000, type=int, help="The path of dev set.")
-parser.add_argument("--strategy", default="mean", type=str, help="The path of dev set.")
-parser.add_argument("--mixed_precision", action="store_true", help="The path of dev set.")
-parser.add_argument("--sd_prompt", default=None, type=str, help="The path of dev set.")
-parser.add_argument("--input_selection_url", default=None, type=str, help="The path of dev set.")
-parser.add_argument("--input_selection_name", default=None, type=str, help="The path of dev set.")
-parser.add_argument("--input_selection_model_type", default=None, type=str, help="The path of dev set.")
-parser.add_argument("--input_selection_model_path", default=None, type=str, help="The path of dev set.")
+    #filter and noise
+    #parser.add_argument("--noise_type",default=None,type=str,help="")
+    #parser.add_argument("--mean",default=None,type=float,help="")
+    #parser.add_argument("--std",default=None,type=float,help="")
+    parser.add_argument("--salt_prob",default=None,type=float,help="")
+    parser.add_argument("--filter_type",default=None,type=str,help="")
+    parser.add_argument("--kernel_size",default=None,type=float,help="")
+    #
 
-#filter and noise
-#parser.add_argument("--noise_type",default=None,type=str,help="")
-#parser.add_argument("--mean",default=None,type=float,help="")
-#parser.add_argument("--std",default=None,type=float,help="")
-#parser.add_argument("--salt_prob",default=None,type=float,help="")
-#parser.add_argument("--filter_type",default=None,type=str,help="")
-#parser.add_argument("--kernel_size",default=None,type=float,help="")
-#
+    #UNet
+    parser.add_argument("--noise_type",default=None,type=str,help="")
+    parser.add_argument("--mean",default=None,type=float,help="")
+    parser.add_argument("--std",default=None,type=float,help="")
+    parser.add_argument('-b', '--batchsize', metavar='B', type=int, nargs='?', default=16,help='Batch size', dest='batchsize')
+    parser.add_argument('-l', '--learningrate', metavar='LR', type=float, nargs='?', default=0.0001,help='Learning rate', dest='lr')
+    #
 
-#UNet
-parser.add_argument("--noise_type",default=None,type=str,help="")
-parser.add_argument("--mean",default=None,type=float,help="")
-parser.add_argument("--std",default=None,type=float,help="")
-parser.add_argument('-b', '--batchsize', metavar='B', type=int, nargs='?', default=16,help='Batch size', dest='batchsize')
-#
+    args = parser.parse_args()
 
-args = parser.parse_args()
+    #UNet
+    train_loader, val_loader = get_cifar10_dataloaders(args.batchsize, args.mean, args.std)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = UNet().to(device)
+    optimizer_Unet= optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    criterion_UNet = torch.nn.MSELoss()
+    #
 
-#UNet
-train_loader, val_loader = get_cifar10_dataloaders(args.batch_size, args.mean, args.std)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = UNet().to(device)
-#
+    args.cur_model = get_model(args.model_type,args.model_path_,args)
+    image0, gt_noise = get_image0(args)
+    image0 = image0.detach()
+    image0_Unet=image0.clone()
+    #image0=image0.squeeze(0).cpu().numpy().transpose(1, 2, 0)  
+    #image0 = cv2.GaussianBlur(image0, (5, 5), 0)
+    #image0 = torch.tensor(image0.transpose(2, 0, 1)).unsqueeze(0).cuda().float() 
+    init_noise = get_init_noise(args,args.model_type,args.cur_model,bs=args.bs)
 
-args.cur_model = get_model(args.model_type,args.model_path_,args)
-image0, gt_noise = get_image0(args)
-image0 = image0.detach()
-#image0=image0.squeeze(0).cpu().numpy().transpose(1, 2, 0)  
-#image0 = cv2.GaussianBlur(image0, (5, 5), 0)
-#image0 = torch.tensor(image0.transpose(2, 0, 1)).unsqueeze(0).cuda().float() 
-init_noise = get_init_noise(args,args.model_type,args.cur_model,bs=args.bs)
+    if args.model_type in ["sd"]:
+        cur_noise = torch.nn.Parameter(torch.tensor(init_noise)).cuda()
+        optimizer = torch.optim.Adam([cur_noise], lr=args.lr)
+    elif args.model_type in ["sd_unet"]:
+        args.cur_model.unet.eval()
+        args.cur_model.vae.eval()
+        cur_noise_0 = torch.nn.Parameter(torch.tensor(init_noise[0])).cuda()
+        optimizer = torch.optim.Adam([cur_noise_0], lr=args.lr)
+    else:
+        cur_noise = torch.nn.Parameter(torch.tensor(init_noise)).cuda()
+        optimizer = torch.optim.Adam([cur_noise], lr=args.lr)
+        
+    if args.distance_metric == "l1":
+        criterion = torch.nn.L1Loss(reduction='none')
+    elif args.distance_metric == "l2":
+        criterion = torch.nn.MSELoss(reduction='none')
+    elif args.distance_metric == "ssim":
+        criterion = SSIMLoss().cuda()
+    elif args.distance_metric == "psnr":
+        criterion = psnr
+    elif args.distance_metric == "lpips":
+        criterion = lpips_fn
+        
+    import time
+    args.measure = float("inf")
 
-if args.model_type in ["sd"]:
-    cur_noise = torch.nn.Parameter(torch.tensor(init_noise)).cuda()
-    optimizer = torch.optim.Adam([cur_noise], lr=args.lr)
-elif args.model_type in ["sd_unet"]:
-    args.cur_model.unet.eval()
-    args.cur_model.vae.eval()
-    cur_noise_0 = torch.nn.Parameter(torch.tensor(init_noise[0])).cuda()
-    optimizer = torch.optim.Adam([cur_noise_0], lr=args.lr)
-else:
-    cur_noise = torch.nn.Parameter(torch.tensor(init_noise)).cuda()
-    optimizer = torch.optim.Adam([cur_noise], lr=args.lr)
+    args.measureUnet = float("inf")
     
-if args.distance_metric == "l1":
-    criterion = torch.nn.L1Loss(reduction='none')
-elif args.distance_metric == "l2":
-    criterion = torch.nn.MSELoss(reduction='none')
-elif args.distance_metric == "ssim":
-    criterion = SSIMLoss().cuda()
-elif args.distance_metric == "psnr":
-    criterion = psnr
-elif args.distance_metric == "lpips":
-    criterion = lpips_fn
-    
-import time
-args.measure = float("inf")
 
-if args.mixed_precision:
-    scaler = GradScaler()
-for i in range(0,1000):
-    start_time = time.time()
-    model.train()
-    print("step:",i)
-    
     if args.mixed_precision:
-        with torch.autocast(device_type='cuda', dtype=torch.float16):
+        scaler = GradScaler()
+    for i in range(0,1000):
+        start_time = time.time()
+
+    #Unet
+        global_step = 0
+        model.train()
+        Unet_epoch_loss = 0
+    #
+
+        print("step:",i)
+
+        #Training Unet
+        for batch in train_loader:
+                src = batch['src']
+                target = batch['target']
+                src = src.to(device=device, dtype=torch.float32)
+                target = target.to(device=device, dtype=torch.float32)
+
+                optimizer_Unet.zero_grad()
+                src_pred = model(src)
+                loss_Unet = criterion_UNet(src_pred, target)
+                Unet_epoch_loss += loss_Unet.item()
+                loss_Unet.backward()
+                optimizer_Unet.step()
+
+                if global_step % 100 == 0:
+                    print('Global step:', global_step, ' Loss:', loss_Unet.item())
+                global_step += 1   
+        #
+    #loss of unet
+        print(f'Epoch {i + 1}/{1000}, Loss: {Unet_epoch_loss / len(train_loader)}')
+        image_Unet=image0_Unet.clone()
+        image_Unet=predict_image_tensor(model,image_Unet,device)
+    #
+
+        if args.mixed_precision:
+            with torch.autocast(device_type='cuda', dtype=torch.float16):
+                image = from_noise_to_image(args,args.cur_model,cur_noise,args.model_type)
+                loss = criterion(image0,image).mean()
+
+                loss_Unet=criterion_UNet(image0_Unet,image).mean()
+        else:
             image = from_noise_to_image(args,args.cur_model,cur_noise,args.model_type)
-            loss = criterion(image0,image).mean()
-    else:
-        image = from_noise_to_image(args,args.cur_model,cur_noise,args.model_type)
-        loss = criterion(image0.detach(),image).mean()
+            loss = criterion(image0.detach(),image).mean()
 
-    if i%100==0:
-        epoch_num_str=str(i)
-        with torch.no_grad():
-            save_img_tensor(image,"./result_imgs/image_cur_"+args.input_selection+"_"+args.distance_metric+"_"+str(args.lr)+"_bs"+str(args.bs)+epoch_num_str+"_"+".png")
+            loss_Unet=criterion_UNet(image0_Unet.detach(),image).mean()
 
-    min_value = criterion(image0,image).mean(-1).mean(-1).mean(-1).min()
-    mean_value = criterion(image0,image).mean()
+        if i%100==0:
+            epoch_num_str=str(i)
+            with torch.no_grad():
+    #save Unet image
+                save_img_tensor(image0_Unet,"./result_imgs/image0_possible_ori"+epoch_num_str+"_"+".png")
+    #
+                save_img_tensor(image,"./result_imgs/image_cur_"+args.input_selection+"_"+args.distance_metric+"_"+str(args.lr)+"_bs"+str(args.bs)+epoch_num_str+"_"+".png")
 
-    if (args.strategy == "min") and (min_value < args.measure):
-        args.measure = min_value
-    if (args.strategy == "mean") and (mean_value < args.measure):
-        args.measure = mean_value
-    print("lowest loss now:",args.measure.item())
+        min_value = criterion(image0,image).mean(-1).mean(-1).mean(-1).min()
+        mean_value = criterion(image0,image).mean()
 
-    if args.distance_metric == "lpips":
-        loss = loss.mean()
-    print("loss "+args.input_selection+" "+args.distance_metric+":",loss.item())
+        min_value_Unet=criterion_UNet(image0_Unet,image).mean(-1).mean(-1).mean(-1).min()
+        mean_value_Unet=criterion_UNet(image0_Unet,image).mean()
 
-    if args.mixed_precision:
-        optimizer.zero_grad()
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
-    else:
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        if (args.strategy == "min") and (min_value < args.measure):
+            args.measure = min_value
 
-    end_time = time.time()
-    print("time for one iter: ",end_time-start_time)
-    torch.cuda.empty_cache()
+            args.measureUnet = torch.tensor(min_value_Unet)
+
+        if (args.strategy == "mean") and (mean_value < args.measure):
+            args.measure = mean_value
+
+            args.measureUnet = torch.tensor(mean_value_Unet)
+
+        print("lowest loss now:",args.measure.item())
+
+        print("lowest Unet loss now:",args.measureUnet.item())
+
+        if args.distance_metric == "lpips":
+            loss = loss.mean()
+        print("loss "+args.input_selection+" "+args.distance_metric+":",loss.item())
+        
+        #
+        print("Unet Loss:",loss_Unet.item())
+#
+        if args.mixed_precision:
+            optimizer.zero_grad()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+        else:
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        end_time = time.time()
+        print("time for one iter: ",end_time-start_time)
+        torch.cuda.empty_cache()
 
 
-cv2_img0 = (image0.squeeze(0).permute(1, 2, 0).cpu().numpy()* 255).astype(np.uint8)
-cv2_img0 = cv2.cvtColor(cv2_img0, cv2.COLOR_BGR2GRAY)
+    cv2_img0 = (image0.squeeze(0).permute(1, 2, 0).cpu().numpy()* 255).astype(np.uint8)
+    cv2_img0 = cv2.cvtColor(cv2_img0, cv2.COLOR_BGR2GRAY)
 
-print("*"*80)
-print("final lowest loss: ",args.measure.item())
-print("2D-entropy-based complexity: ", complexity.calcEntropy2dSpeedUp(cv2_img0, 3, 3))
+    print("*"*80)
+    print("final lowest loss: ",args.measure.item())
 
-if args.write_txt_path:
-    with open(args.write_txt_path,"a") as f:
-        f.write(str(args.measure.item())+"\n")
+    print("final lowest Unet loss: ",args.measureUnet.item())
 
-if args.sd_prompt:
-    save_img_tensor(image0,"./result_imgs/ORI_"+args.sd_prompt+args.distance_metric+"_"+str(args.lr)+"_bs"+str(args.bs)+epoch_num_str+"_"+".png")
-    save_img_tensor(image,"./result_imgs/last_"+args.sd_prompt+args.distance_metric+"_"+str(args.lr)+"_bs"+str(args.bs)+epoch_num_str+"_"+".png")
-if args.input_selection_url:
-    save_img_tensor(image0,"./result_imgs/ORI_"+args.input_selection_url.split("/")[-1]+args.distance_metric+"_"+str(args.lr)+"_bs"+str(args.bs)+epoch_num_str+"_"+".png")
-    save_img_tensor(image,"./result_imgs/last_"+args.input_selection_url.split("/")[-1]+args.distance_metric+"_"+str(args.lr)+"_bs"+str(args.bs)+epoch_num_str+"_"+".png")
+    print("2D-entropy-based complexity: ", complexity.calcEntropy2dSpeedUp(cv2_img0, 3, 3))
+
+    if args.write_txt_path:
+        with open(args.write_txt_path,"a") as f:
+            f.write(str(args.measure.item())+"\n")
+
+    if args.sd_prompt:
+        save_img_tensor(image0,"./result_imgs/ORI_"+args.sd_prompt+args.distance_metric+"_"+str(args.lr)+"_bs"+str(args.bs)+epoch_num_str+"_"+".png")
+        save_img_tensor(image,"./result_imgs/last_"+args.sd_prompt+args.distance_metric+"_"+str(args.lr)+"_bs"+str(args.bs)+epoch_num_str+"_"+".png")
+    if args.input_selection_url:
+        save_img_tensor(image0,"./result_imgs/ORI_"+args.input_selection_url.split("/")[-1]+args.distance_metric+"_"+str(args.lr)+"_bs"+str(args.bs)+epoch_num_str+"_"+".png")
+        save_img_tensor(image,"./result_imgs/last_"+args.input_selection_url.split("/")[-1]+args.distance_metric+"_"+str(args.lr)+"_bs"+str(args.bs)+epoch_num_str+"_"+".png")
+    
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    main()
